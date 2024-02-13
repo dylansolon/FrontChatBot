dd8e6cad3b895e96d4173a30e8f49c1bd7539d0f
root <root@DylanSolon>
2024-02-13 15:52:30 +0100

    test

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import toto from './toto.png';
 
 import './index.scss';
 
-const hello = 'hello';
-document.body.innerHTML = ` 
-  <h1>Hello Toto !</h1>
-  <img width="300" src="${toto}" alt="toto">
+document.body.innerHTML = `
+<h1>Hello Toto !</h1>
+<h2>dylan</h2>
+<img width="300" src="${toto}" alt="toto">
 `;
-
-console.log(hello);

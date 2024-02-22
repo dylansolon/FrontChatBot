10fc4709c8a7e41d6d94a2be6a137abdeb84d27b
Mattoucoding <mattcoding45@gmail.com>
2024-02-22 09:58:05 +0100

    Changement des images + modification de page.js

--- src/views/page.js
+++ src/views/page.js
@@ -1,11 +1,12 @@
-import toto from '../assets/img/toto.jpg';
-import robot from '../assets/img/robot.png';
-import naruto from '../assets/img/naruto.jpg';
+import icon from '../assets/img/robot.png';
+import bot1 from '../assets/img/bulbizarre.jpg';
+import bot2 from '../assets/img/salameche.jpg';
+import bot3 from '../assets/img/carapuce.jpg';
 
 export default () => (`
 <header>
     <nav>
-        <img src="robot.png" alt=""/>
+        <img src="${icon}" alt="Icon Header"/>
         <h1>Chatbot.io</h1>
     </nav>
 </header>
@@ -19,9 +20,9 @@ export default () => (`
         <div class="leftItem" id="Bot-one">
             <div class="left__robotContainer">
                 <div class="robot-icon">
-                    <img src="${toto}" alt="Robot Icon">
+                    <img src="${bot1}" alt="Robot Icon">
                 </div>
-                <h1>Goku Bot</h1>
+                <h1>Bulbizarre</h1>
             </div>
             <p>Lorem, ipsum dolor.</p>
             <p>2min</p>
@@ -29,9 +30,9 @@ export default () => (`
         <div class="leftItem" id="Bot-two">
             <div class="left__robotContainer">
                 <div class="robot-icon">
-                    <img src="${robot}" alt="Robot Icon">
+                    <img src="${bot2}" alt="Robot Icon">
                 </div>
-                <h1>Luffy Bot</h1>
+                <h1>Salameche</h1>
             </div>
             <p>Lorem, ipsum dolor.</p>
             <p>2min</p>
@@ -39,9 +40,9 @@ export default () => (`
         <div class="leftItem" id="Bot-three">
             <div class="left__robotContainer">
                 <div class="robot-icon">
-                    <img src="${naruto}" alt="Robot Icon">
+                    <img src="${bot3}" alt="Robot Icon">
                 </div>
-                <h1>Naruto Bot</h1>
+                <h1>Carapuce</h1>
             </div>
             <p>Lorem, ipsum dolor.</p>
             <p>2min</p>
@@ -52,7 +53,7 @@ export default () => (`
     <section class="right">
         <div class="containerRight" id="conv1">
             <div class="containerRightTitle">
-                <h1>Robot français</h1>
+                <h1>Nom du bot</h1>
             </div>
             <ul class="chatbox">
                 <li class="myTurn">
@@ -62,7 +63,7 @@ export default () => (`
                     </div>
                 </li>
                 <li class="botTurn">
-                    <h2 class="robot-name" style="font-size: 0.8rem;">Robot français</h2> <!-- Robot's name with smaller font size -->
+                    <h2 class="robot-name" style="font-size: 0.8rem;">Nom du bot</h2> <!-- Robot's name with smaller font size -->
                     <div class="message-container">
                         <span class="message">Bot's message here</span>
                         <span class="timestamp">12:30 PM, 16 Feb 2024</span>

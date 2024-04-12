5a93c07416c824e0ff7098d18a5588f225d940fd
dylan <solon.dylan@laposte.net>
2024-04-12 09:22:33 +0200

    fix: chatbotjs

--- src/controllers/chatbot.js
+++ src/controllers/chatbot.js
@@ -22,11 +22,6 @@ class ChatBot {
     return response.data;
   }
 
-  async fetchUsersData() {
-    const response = await axios.get('http://127.0.0.1/user/1');
-    return response.data;
-  }
-
   eventSendMessage() {
     const sendButton = document.querySelector('.ri-send-plane-fill');
     const messageInput = document.querySelector('.input');
@@ -58,11 +53,9 @@ class ChatBot {
   async run() {
     const bots = await this.fetchBotsData();
     const messages = await this.fetchMessagesData();
-    const users = await this.fetchUsersData();
     const data = {
       bots,
-      messages,
-      users
+      messages
     };
     this.el.innerHTML = this.render(data);
 
@@ -70,7 +63,7 @@ class ChatBot {
     const chatbox = document.querySelector('.chatbox');
 
     // Afficher les messages dans la boîte de chat
-    messages.forEach(message => {
+    messages.forEach((message) => {
       const messageHTML = `
       <li class="${message.userId ? 'myTurn' : 'botTurn'}">
         <h2 class="robot-name">${message.userId ? '' : message.botName}</h2>
@@ -87,15 +80,16 @@ class ChatBot {
     this.eventSendMessage();
     this.scrollChatboxToBottom();
   }
+
   async sendMessage(chatbox, messageInput) {
     const userMessage = messageInput.value.trim();
 
     if (userMessage !== '') {
-        const now = new Date();
-        const timestamp = `${now.getHours()}:${String(now.getMinutes()).padStart(2, '0')} ${now.getDate()}/${now.getMonth() + 1}/${now.getFullYear()}`;
+      const now = new Date();
+      const timestamp = now.toISOString().slice(0, 19).replace('T', ' ');
 
-        // Création du nouveau message de l'utilisateur
-        const newMessage = `
+      // Création du nouveau message de l'utilisateur
+      const newMessage = `
             <li class="myTurn">
                 <div class="message-container">
                     <span class="message">${userMessage}</span>
@@ -104,84 +98,63 @@ class ChatBot {
             </li>
         `;
 
-        // Ajout du message de l'utilisateur à la boîte de chat
-        chatbox.innerHTML += newMessage;
-        messageInput.value = '';
+      // Ajout du message de l'utilisateur à la boîte de chat
+      chatbox.innerHTML += newMessage;
+      messageInput.value = '';
 
-        // Envoi du message de l'utilisateur au serveur
-        await axios.post('http://127.0.0.1/messages', {
-            botId: null,
-            botName: '',
-            userId: 1,
-            text: userMessage
-        });
+      // Envoi du message de l'utilisateur au serveur
+      await axios.post('http://127.0.0.1/messages', {
+        botId: null,
+        botName: '',
+        userId: 1,
+        text: userMessage
+      });
 
-        // Récupération des données des bots depuis le serveur
-        const bots = await this.fetchBotsData();
-        const botResponses = [];
+      // Récupération des données des bots depuis le serveur
+      const bots = await this.fetchBotsData();
+      const botResponses = [];
 
-        // Parcours de tous les bots
-        for (const bot of bots) {
-            // Vérification des actions du bot
-            if (bot.actions) {
-                // Parcours de toutes les actions du bot
-                for (const action of bot.actions) {
-                    // Vérification si l'action correspond au message de l'utilisateur
-                    if (action.words && action.words.includes(userMessage.toLowerCase())) {
-                        // Création de la réponse du bot
-                        const botResponse = `
-                            <li class="botTurn">
-                                <h2 class="robot-name">${bot.name}</h2>
-                                <div class="message-container">
-                                    <span class="message">${action.response}</span>
-                                    <span class="timestamp">${timestamp}</span>
-                                </div>
-                            </li>
-                        `;
-                        // Ajout de la réponse du bot au tableau des réponses
-                        botResponses.push(botResponse);
+      await Promise.all(bots.map(async (bot) => {
+        // Vérification des actions du bot
+        if (bot.actions) {
+          // Parcours de toutes les actions du bot
+          await Promise.all(bot.actions.map(async (action) => {
+            // Vérification si l'action correspond au message de l'utilisateur
+            if (action.words && action.words.includes(userMessage.toLowerCase())) {
+              // Création de la réponse du bot
+              const botResponse = `
+                        <li class="botTurn">
+                            <h2 class="robot-name">${bot.name}</h2>
+                            <div class="message-container">
+                                <span class="message">${action.response}</span>
+                                <span class="timestamp">${timestamp}</span>
+                            </div>
+                        </li>
+                    `;
+              // Ajout de la réponse du bot au tableau des réponses
+              botResponses.push(botResponse);
 
-                        // Envoi de la réponse du bot au serveur
-                        await axios.post('http://127.0.0.1/messages', {
-                            botId: bot.id,
-                            botName: bot.name,
-                            userId: null,
-                            text: action.response
-                        });
-                    }
-                }
-            }
-        }
-
-        // Si des réponses ont été trouvées, les ajouter à la boîte de chat
-        if (botResponses.length > 0) {
-            chatbox.innerHTML += botResponses.join('');
-        } else {
-            // Sinon, afficher une réponse par défaut
-            const defaultResponse = `
-                <li class="botTurn">
-                    <h2 class="robot-name">Bot par défaut</h2>
-                    <div class="message-container">
-                        <span class="message">Désolé, je n'ai pas compris.</span>
-                        <span class="timestamp">${timestamp}</span>
-                    </div>
-                </li>
-            `;
-            chatbox.innerHTML += defaultResponse;
-
-            // Envoi de la réponse par défaut au serveur
-            await axios.post('http://127.0.0.1/messages', {
-                botId: null,
-                botName: '',
+              // Envoi de la réponse du bot au serveur
+              await axios.post('http://127.0.0.1/messages', {
+                botId: bot.id,
+                botName: bot.name,
                 userId: null,
-                text: "Désolé, je n'ai pas compris."
-            });
+                text: action.response
+              });
+            }
+          }));
         }
+      }));
 
-        // Faire défiler la boîte de chat jusqu'en bas
-        this.scrollChatboxToBottom();
+      // Si des réponses ont été trouvées, les ajouter à la boîte de chat
+      if (botResponses.length > 0) {
+        chatbox.innerHTML += botResponses.join('');
+      }
+
+      // Faire défiler la boîte de chat jusqu'en bas
+      this.scrollChatboxToBottom();
     }
-}
+  }
 
   scrollChatboxToBottom() {
     const chatbox = document.querySelector('.chatbox');
